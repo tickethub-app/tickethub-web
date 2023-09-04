a7c64ed364329339e8078b95734211fcd10cc16d
Saiete, Luis <lsaiete@deloitte.co.mz>
2023-09-05 00:02:34 +0200

    Create event details page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { ThemeProvider } from 'styled-components';
 import Home from './pages/Home';
 import { GlobalStyle } from './styles/global';
 import { theme } from './styles/theme';
+import EventDetails from './pages/EventDetails';
 
 function App() {
 	return (
 		<>
 			<ThemeProvider theme={theme}>
-				<Home />
+				{/* <Home /> */}
+				<EventDetails />
 				<GlobalStyle />
 			</ThemeProvider>
 		</>
---
+++ src/pages/EventDetails/index.tsx
@@ -0,0 +1,52 @@
+export default function EventDetails() {
+	return (
+		<>
+			<header>
+				<div>
+					<span>icon</span>
+					<h2>Event details</h2>
+				</div>
+				<img src="" alt="" />
+			</header>
+
+			<main>
+				<h1>Data wave</h1>
+
+				<section>
+					<img src="" alt="" />
+					<div>
+						<strong>Mozdevz</strong>
+						<small>Organizer</small>
+					</div>
+				</section>
+
+				<section>
+					<img src="" alt="" />
+					<div>
+						<strong>9 July, 2023</strong>
+						<small>Saturday, 8AM-3PM</small>
+					</div>
+				</section>
+
+				<section>
+					<img src="" alt="" />
+					<div>
+						<strong>Feng UEM</strong>
+						<small>FENG UEM</small>
+					</div>
+				</section>
+
+				<a href="">RSVP</a>
+			</main>
+
+			<aside>
+				<h2>Description</h2>
+				<p>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis, temporibus assumenda id
+					numquam at sit nobis, illum veniam eius odit, porro atque officia provident incidunt accusamus! Doloremque,
+					expedita sed.
+				</p>
+			</aside>
+		</>
+	);
+}
