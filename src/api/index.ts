ad3e7221a419410252275b11b57b7c54924890ef
Luis Saiete <luistsaiete@gmail.com>
2023-09-07 00:25:45 +0200

    Connect to API

---
+++ src/api/index.ts
@@ -0,0 +1,14 @@
+import axios from 'redaxios';
+
+export const api = axios.create({
+	baseURL: 'http://localhost:5000/api/v1'
+});
+
+export function getItems(url: string): Promise<any> {
+	const response = api
+		.get(url)
+		.then((response) => response.data)
+		.catch((error) => ({ data: [] }));
+
+	return response;
+}
